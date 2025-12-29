0ca9f2022f6de96da155763bcbf70fa8135a7da2
e-dantas <evelindantas@proton.me>
2025-12-29 17:06:30 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function resultados(vitorias, derrotas){
+    return vitorias - derrotas;
+} 
+
+function categorias(saldoVitorias){
+    if(saldoVitorias <= 10){
+        return "Ferro";
+    } else if(saldoVitorias <= 20){
+        return "Bronze";
+    } else if(saldoVitorias <= 50){
+        return "Prata";
+    } else if(saldoVitorias <= 80){
+        return "Ouro";
+    } else if(saldoVitorias <= 90){
+        return "Diamante";
+    } else if(saldoVitorias <= 100){
+        return "Lendário";
+    } else {
+        return "Imortal";
+    }
+}
+
+let saldoVitorias = resultados(101, 0);
+let nivel = categorias(saldoVitorias);
+
+console.log(`O Herói tem saldo de ${saldoVitorias} e está no nível de ${nivel}`);
+
