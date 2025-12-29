317caa9cf886bd24a3a17d502d3a1aa9a526b421
e-dantas <evelindantas@proton.me>
2025-12-29 19:20:11 -0300

    quarto commit

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function resultados(vitorias, derrotas){
 function classificacao(saldoVitorias){
     if(saldoVitorias <= 10){
         return "Ferro";
-    } else if(saldoVitorias <= 20){
+    } else if (saldoVitorias <= 20){
         return "Bronze";
-    } else if(saldoVitorias <= 50){
+    } else if (saldoVitorias <= 50){
         return "Prata";
-    } else if(saldoVitorias <= 80){
+    } else if (saldoVitorias <= 80){
         return "Ouro";
-    } else if(saldoVitorias <= 90){
+    } else if (saldoVitorias <= 90){
         return "Diamante";
-    } else if(saldoVitorias <= 100){
+    } else if (saldoVitorias <= 100){
         return "Lendário";
     } else {
         return "Imortal";
